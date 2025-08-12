fixing some numerical stability issues and adding train mode
addind some initialization in linear layers, addind a more stable sigmoid
adding train mode for dropout

NN.py
@@ -6,22 +6,22 @@ import random as r
 class Linear:
     # this class is copied from my Transformer backward project
     def __init__(self, in_dim, out_dim, scale=1.0):
-        self.W = np.random.randn(in_dim, out_dim).astype(np.float32) * scale
+        self.W = self.xavier_init(in_dim, out_dim)
         self.b = np.zeros((out_dim,), dtype=np.float32)
         # gradients
         self.dW = np.zeros_like(self.W) #same dim as W
         self.db = np.zeros_like(self.b) #same dim as b
         self.cache = None #for backward we cache some data during forward
+    
+    def xavier_init(self, in_dim, out_dim):
+        #initilize with xavier like method in order to respect numerical stability
+        std = np.sqrt(2.0 / (in_dim + out_dim))
+        return (np.random.randn(in_dim, out_dim).astype(np.float32) * std)
 
     def forward(self, x):
-        # x shape: (b, seq, in_dim)  OR (N, in_dim) N=batch*seq
         self.x_shape = x.shape
-        # out = x @ self.W + self.b
-        # it does the same thing as flattening and then doing the operation
-        # but it is restrictive for dimensions
-        # it will be simplier to do the backpropagation if we used the flattened (reduced to 2 dim) tensors
-        x_flat = x.reshape(-1, x.shape[-1])  # (N, in_dim)
-        out_flat = x_flat @ self.W + self.b  # (N, out_dim)
+        x_flat = x.reshape(-1, x.shape[-1])  
+        out_flat = x_flat @ self.W + self.b  
         out = out_flat.reshape(*x.shape[:-1], self.W.shape[1])
         # cache x_flat for backward
         self.cache = x_flat
@@ -29,12 +29,12 @@ class Linear:
 
     def backward(self, dout):
         # we reflatten the output
-        dout_flat = dout.reshape(-1, dout.shape[-1])  # (N, out_dim)
+        dout_flat = dout.reshape(-1, dout.shape[-1]) 
         x_flat = self.cache  # (N, in_dim) #we use the flatten x that we cached in forward 
         # grads
-        self.dW[:] = x_flat.T @ dout_flat # copy (in_dim, out_dim)
-        self.db[:] = dout_flat.sum(axis=0) #copy (out_dim,)
-        dx_flat = dout_flat @ self.W.T # (N, in_dim)
+        self.dW[:] = x_flat.T @ dout_flat 
+        self.db[:] = dout_flat.sum(axis=0) 
+        dx_flat = dout_flat @ self.W.T 
         dx = dx_flat.reshape(*dout.shape[:-1], self.W.shape[0])
         return(dx)
 
@@ -42,13 +42,17 @@ class Linear:
         #get
         return([(self.W, self.dW, 'W'), (self.b, self.db, 'b')])
     
-class dropout:
+class Dropout:
     # we take the output of a layer and randomly put the value to 0 with respect to probability p
-    def __init__(self, p):
+    def __init__(self, p, training=True):
         self.p = p
         self.cache = None #fro backward
+        self.training = training
 
     def forward(self, x):
+        if not self.training: #if we are not in training , just go through
+            self.cache = np.ones_like(x)
+            return(x)
         #entry: (b, seq, in_dim) or (N, out_dim)
         mask = np.random.binomial(1, 1 - self.p, size=x.shape).astype(x.dtype)
         #we create a mask that randomly 0es some elements in tensor x 
@@ -80,13 +84,17 @@ class Sigmoid:
     def __init__(self):
         self.cache = None #for backward
     @staticmethod
-    def sigmoid(x): 
-        #https://stackoverflow.com/questions/51976461/optimal-way-of-defining-a-numerically-stable-sigmoid-function-for-a-list-in-pyth
-        return np.where(
-                x >= 0, # condition
-                1 / (1 + np.exp(-x)), # For positive values
-                np.exp(x) / (1 + np.exp(x)) # For negative values
-                )
+    def sigmoid(x):
+        # we use this sigmoid for numerical stability
+        # a previous simplier version was overflowing
+        out = np.empty_like(x, dtype=np.float64)   # use float64 for numerical stability
+        pos = x >= 0
+        neg = ~pos
+        # compute only where needed
+        out[pos] = 1.0 / (1.0 + np.exp(-x[pos]))
+        exp_x = np.exp(x[neg])
+        out[neg] = exp_x / (1.0 + exp_x)
+        return out.astype(x.dtype)
     
     def forward(self, x):
         out = self.sigmoid(x)
@@ -97,7 +105,7 @@ class Sigmoid:
         # dy/dx = y(1-y)
         return(dout*(self.cache*(1-self.cache)))
     
-class tanh:
+class Tanh:
     def __init__(self):
         self.cache = None #for backward
     